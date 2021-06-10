45dc38a52d48a4d836a2cf6e27d20a4137787288
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-06-10 17:39:01 -0300

    Base roman to arabic conversion

--- RomanNumerals/RomanNumerals/RomanToArabicNumber.cs
+++ RomanNumerals/RomanNumerals/RomanToArabicNumber.cs
@@ -1,18 +1,34 @@
 ﻿
+using System.Collections.Generic;
+
 namespace RomanNumerals
 {
     public class RomanToArabicNumber
     {
         private string RomanNumeral;
+        private Dictionary<string, int> RomanArabicPairs;
 
         public RomanToArabicNumber(string romanNumeral)
         {
             RomanNumeral = romanNumeral;
+            RomanArabicPairs = new Dictionary<string, int>
+            {
+                {"I",1},
+                {"V",5},
+                {"X",10},
+                {"L",50},
+                {"C",100},
+                {"D",500},
+                {"M",100}
+             };
         }
 
         public int Convert()
         {
-            return 1;
+            if (RomanArabicPairs.TryGetValue(RomanNumeral, out int value))
+                return value;
+
+            return 0;
         }
     }
 }
