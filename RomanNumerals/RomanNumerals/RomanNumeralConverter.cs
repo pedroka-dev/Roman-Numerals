02c3c8b7b2034dd351b3fb559c4a415f8e847dbe
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-06-10 15:06:28 -0300

    Basic conversion to roman numerals

--- RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
@@ -8,6 +8,61 @@ namespace RomanNumerals
 {
     public class RomanNumeralConverter
     {
-        
+        private int arabicNumber;
+
+        public RomanNumeralConverter(int arabicNumber)
+        {
+            this.arabicNumber = arabicNumber;
+        }
+
+        public string Convert()
+        {
+            string romanNumeral = "";
+
+            romanNumeral = integerToRomanNumeral(arabicNumber);
+
+            return romanNumeral;
+        }
+
+        public string integerToRomanNumeral(int number)
+        {
+            if (number >= 1000)
+            {
+                return "M";
+            }
+
+            else if (number >= 500)
+            {
+                return "D";
+            }
+
+            else if (number >= 100)
+            {
+                return "C";
+            }
+
+            else if (number >= 50)
+            {
+                return "L";
+            }
+
+            else if (number >= 10)
+            {
+                return "X";
+            }
+
+            else if (number >= 5)
+            {
+                return "V";
+            }
+            else if(number >= 1)
+            {
+                return "I";
+            }
+            else
+            {
+                return "I";
+            }
+        }
     }
 }
