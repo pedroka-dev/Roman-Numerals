225181453820e66560e4e4a4a3aa98eebaa92376
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-06-11 13:00:53 -0300

    Treatment for numbers bellow 4000

--- RomanNumerals/RomanNumerals/RomanToArabicNumber.cs
+++ RomanNumerals/RomanNumerals/RomanToArabicNumber.cs
@@ -19,16 +19,37 @@ namespace RomanNumerals
                 {"L",50},
                 {"C",100},
                 {"D",500},
-                {"M",100}
+                {"M",1000},
+                {"Ī",1000},
+                {"V̄",5000},
+                {"X̄",10000}
              };
         }
 
         public int Convert()
         {
-            if (RomanArabicPairs.TryGetValue(RomanNumeral, out int value))
-                return value;
+            var result = 0;
 
-            return 0;
+            foreach (var letter in RomanNumeral)
+            {
+                if(RomanArabicPairs.TryGetValue(letter.ToString(), out int value))
+                result += value;
+            }
+
+            if (RomanNumeral.Contains("IV") || RomanNumeral.Contains("IX"))
+                result -= 2;
+
+            if (RomanNumeral.Contains("XL") || RomanNumeral.Contains("XC"))
+                result -= 20;
+
+            if (RomanNumeral.Contains("CD") || RomanNumeral.Contains("CM"))
+                result -= 200;
+
+            if (RomanNumeral.Contains("ĪV̄") || RomanNumeral.Contains("ĪX̄"))
+                result -= 2000;
+
+
+                return result;
         }
     }
 }
